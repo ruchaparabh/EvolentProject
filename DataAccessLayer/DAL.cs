cf22e0cde400e381fb6c2bb14611e5be12817dbe
ruchaparabh <62441239+ruchaparabh@users.noreply.github.com>
2020-03-21 15:24:50 +0530

    Add files via upload

---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAccessLayer
+{
+    public static class  DAL
+    {
+        static TestProjectEntities DbContext;
+        static DAL()
+        {
+            DbContext = new TestProjectEntities();
+        }
+        public static List<EmployeeDetail> GetAllProducts()
+        {
+            return DbContext.EmployeeDetails.ToList();
+        }
+        public static EmployeeDetail GetEmployee(int employeeId)
+        {
+            return DbContext.EmployeeDetails.Where(e => e.EmployeeId == employeeId).FirstOrDefault();
+        }
+        public static bool InsertEmployee(EmployeeDetail emp)
+        {
+            bool status;
+            try
+            {
+                DbContext.EmployeeDetails.Add(emp);
+                DbContext.SaveChanges();
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+        public static bool UpdateEmployee(EmployeeDetail emp)
+        {
+            bool status;
+            try
+            {
+                EmployeeDetail empObj = DbContext.EmployeeDetails.Where(e => e.EmployeeId == emp.EmployeeId).FirstOrDefault();
+                if (empObj != null)
+                {
+                    empObj.Firstname = emp.Firstname;
+                    empObj.Lastname = emp.Lastname;
+                    empObj.Email = emp.Email;
+                    empObj.Contact = emp.Contact;
+                    empObj.Status = emp.Status;
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+        public static bool DeleteEmployee(int id)
+        {
+            bool status;
+            try
+            {
+                EmployeeDetail empObj = DbContext.EmployeeDetails.Where(e => e.EmployeeId == id).FirstOrDefault();
+                if (empObj != null)
+                {
+                    DbContext.EmployeeDetails.Remove(empObj);
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+    }
+}
